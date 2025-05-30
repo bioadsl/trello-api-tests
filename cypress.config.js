19870cf797875be3a41aaacd41139a7caa7cc449
fabricio <fabricio.4135@gmail.com>
2025-05-30 04:34:55 -0300

    atualizacao do config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
@@ -7,12 +6,6 @@ module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
       return config;
     },
     specPattern: "cypress/e2e/**/*.feature",
