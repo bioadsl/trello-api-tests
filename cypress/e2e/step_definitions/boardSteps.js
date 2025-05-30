4b1eabfc6599f984ee12dea84050d20403b51d16
fabricio <fabricio.4135@gmail.com>
2025-05-30 07:22:24 -0300

    criacao do .env token e api key

--- cypress/e2e/step_definitions/boardSteps.js
+++ cypress/e2e/step_definitions/boardSteps.js
@@ -3,11 +3,19 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 let boardId;
 
 Given("que desejo criar um novo board", () => {
-  // Preparação (se necessária)
+  // Nenhuma preparação necessária aqui
 });
 
 When("envio uma requisição para criar o board", () => {
-  cy.request("POST", `/1/boards/?name=BoardTest&key=SEU_KEY&token=SEU_TOKEN`).then((response) => {
+  cy.request({
+    method: "POST",
+    url: "/1/boards/",
+    qs: {
+      name: "BoardTest",
+      key: Cypress.env("TRELLO_API_KEY"),
+      token: Cypress.env("TRELLO_API_TOKEN")
+    }
+  }).then((response) => {
     expect(response.status).to.eq(200);
     boardId = response.body.id;
   });
@@ -18,17 +26,41 @@ Then("o board deve ser criado com sucesso", () => {
 });
 
 Given("que tenho um board existente", () => {
-  cy.request("POST", `/1/boards/?name=BoardDeleteTest&key=SEU_KEY&token=SEU_TOKEN`).then((response) => {
+  cy.request({
+    method: "POST",
+    url: "/1/boards/",
+    qs: {
+      name: "BoardDeleteTest",
+      key: Cypress.env("TRELLO_API_KEY"),
+      token: Cypress.env("TRELLO_API_TOKEN")
+    }
+  }).then((response) => {
     boardId = response.body.id;
   });
 });
 
 When("envio uma requisição para excluí-lo", () => {
-  cy.request("DELETE", `/1/boards/${boardId}?key=SEU_KEY&token=SEU_TOKEN`).then((response) => {
+  cy.request({
+    method: "DELETE",
+    url: `/1/boards/${boardId}`,
+    qs: {
+      key: Cypress.env("TRELLO_API_KEY"),
+      token: Cypress.env("TRELLO_API_TOKEN")
+    }
+  }).then((response) => {
     expect(response.status).to.eq(200);
   });
 });
 
 Then("o board deve ser removido com sucesso", () => {
-  // Verificação adicional opcional
+  // Checagem extra, opcional!
+  cy.request({
+    method: "GET",
+    url: `/1/boards/${boardId}`,
+    qs: {
+      key: Cypress.env("TRELLO_API_KEY"),
+      token: Cypress.env("TRELLO_API_TOKEN")
+    },
+    failOnStatusCode: false // evita erro se já estiver 404
+  }).its("status").should("eq", 404);
 });
---
+++ cypress/integration/trello_test.js
@@ -0,0 +1,9 @@
+const apiKey = "SUA_API_KEY";
+const token = "SEU_TOKEN";
+
+cy.request({
+  method: "POST",
+  url: `https://api.trello.com/1/boards/?name=MeuBoard&key=${apiKey}&token=${token}`
+}).then((response) => {
+  expect(response.status).to.eq(200);
+});
